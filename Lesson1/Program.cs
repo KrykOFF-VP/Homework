b29ba1c9de760c270c410e2497758e7e88bcabe7
KrykOFF-VP <krykoff.vp@mail.ru>
2022-04-30 02:12:17 +0300

    Create homework

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lesson1
 {
     internal class Program
-    {
+    {       
         static void Main(string[] args)
         {
+            Console.Write($"Укажите ваше имя: ");                                               // Запрашиваем имя
+            string name = Console.ReadLine();                                                   //Сохраняем полученные данные в промежуточную переменную
+            DateTime mydata = DateTime.Now;                                                     // Создаем переменую, которая получает текущее время
+            Console.WriteLine($"Привет, {name} сегодня {mydata.ToString("dd.MM.yyyy")}");       // Выводим приветствие  и дату
         }
     }
 }
