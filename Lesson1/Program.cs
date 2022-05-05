9a95bbc1a27a1c8573abc7f4795baa799c71ca7b
KrykOFF-VP <krykoff.vp@mail.ru>
2022-05-05 20:26:57 +0300

    Исправил коментарий

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -9,7 +9,7 @@ namespace Lesson1
             Console.Write($"Укажите ваше имя: ");                                               // Запрашиваем имя
             string name = Console.ReadLine();                                                   //Сохраняем полученные данные в промежуточную переменную
             DateTime mydata = DateTime.Now;                                                     // Создаем переменую, которая получает текущее время
-            Console.WriteLine($"Привет, {name} сегодня {mydata.ToString("dd.MM.yyyy")}");       // Выводим приветствие  и дату
+            Console.WriteLine($"Привет, {name} сегодня {mydata.ToString("dd.MM.yyyy")}");       // Выводим приветствие  и дату (ставим формат даты)
         }
     }
 }
