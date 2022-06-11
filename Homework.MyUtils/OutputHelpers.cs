40ab8806ad6e533e46c8ec904375dd40931ff4f8
KrykOFF-VP <krykoff.vp@mail.ru>
2022-06-11 10:34:25 +0300

    Добавление класса приложения

---
+++ Homework.MyUtils/OutputHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.MyUtils
+{
+    public class OutputHelpers
+    {
+        public void PrintInfo(int homeworkNumber, string myname)
+        {
+            Console.WriteLine($"Доашняя работа. Урок{homeworkNumber}");
+            Console.WriteLine($"Студент: {myname}");
+            Console.WriteLine("==========================================")
+        }
+    }
+}
